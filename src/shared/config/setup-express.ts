1ed750ecc3d742fc96cbf25cef5f5ca100c749d3
Willian17 <willianfernandes113@gmail.com>
2021-08-18 18:48:40 -0400

    lib: add cors

--- src/shared/config/setup-express.ts
+++ src/shared/config/setup-express.ts
@@ -1,5 +1,9 @@
 import { Express, json } from 'express'
+import cors from 'cors'
 
 export default (app: Express): void => {
+  app.use(cors(
+    {origin: 'https://quickdates.tech'}
+  ));
   app.use(json());
 }
