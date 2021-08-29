e4122f5d940f1af8df5105af1c4e961a8132291f
Willian17 <willianfernandes113@gmail.com>
2021-08-29 12:53:33 -0400

    deploy heroku 1.9

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,13 +3,14 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": [process.env.NODE_ENV === 'production' ? "**/models/*.js" : "**/models/*.ts" ],
-  "migrations": [process.env.NODE_ENV === 'production' ? "./dist/src/shared/database/migrations/*.js" : "./src/shared/database/migrations/*.ts"],
+  "migrations": [process.env.NODE_ENV === 'production' ? "./dist/shared/database/migrations/*.js" : "./src/shared/database/migrations/*.ts"],
   "cli": {
     "migrationsDir": "./src/shared/database/migrations"
   },
-  "extra": {
+  "extra": process.env.NODE_ENV === 'production' ? {
     "ssl": {
-      "rejectUnauthorized": false
+        "rejectUnauthorized": false
     }
-  }
+    } : {}
+
 }
