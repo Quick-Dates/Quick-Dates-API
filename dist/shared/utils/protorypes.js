4e99506467cbd7352f60f57af05d9304b070392e
Willian17 <willianfernandes113@gmail.com>
2021-10-16 19:25:53 -0400

    statistics tasks from week

---
+++ dist/shared/utils/protorypes.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function prototypes() {
+    Array.prototype.changePosition = function (from, to) {
+        this.splice(to, 0, this.splice(from, 1)[0]);
+        return this;
+    };
+}
+exports.default = prototypes;
+;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJvdG9yeXBlcy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL3NyYy9zaGFyZWQvdXRpbHMvcHJvdG9yeXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQU1BLFNBQXdCLFVBQVU7SUFDaEMsS0FBSyxDQUFDLFNBQVMsQ0FBQyxjQUFjLEdBQUcsVUFBUyxJQUFZLEVBQUUsRUFBVTtRQUNoRSxJQUFJLENBQUMsTUFBTSxDQUFDLEVBQUUsRUFBRSxDQUFDLEVBQUUsSUFBSSxDQUFDLE1BQU0sQ0FBQyxJQUFJLEVBQUUsQ0FBQyxDQUFDLENBQUMsQ0FBQyxDQUFDLENBQUMsQ0FBQztRQUM1QyxPQUFPLElBQUksQ0FBQztJQUNkLENBQUMsQ0FBQTtBQUNILENBQUM7QUFMRCw2QkFLQztBQUFBLENBQUMifQ==
